8bc78391970c44d118dde9b7ed3739fcbd50d857
Samreen <samreenf3112@gmail.com>
2025-06-04 19:02:08 +0530

    Initial commit

---
+++ Backend/Supabase/.env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+// env.d.ts
+export const SUPABASE_URL = "https://siayecqdxwvgagmpkcmq.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpYXllY3FkeHd2Z2FnbXBrY21xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1ODg5NjQsImV4cCI6MjA2NDE2NDk2NH0.WTzCTGgCiZdtTbHUgH8IoqyizpSi2OIor_nsinDzomU";
